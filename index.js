9cfcfabd3553b14420980cefb7d35387f7146395
ACJDG <amadeusjunqueira@gmail.com>
2014-12-06 18:43:51 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+exports = function bindAll(obj) {
+  var funcs = [].slice.call(arguments, 1);
+
+  if (funcs.length === 0) {
+      throw new Error('bindAll must be passed function names');
+    }
+
+  [].forEach.call(funcs, function(f) {
+      obj[f] = Function.prototype.bind.call(obj[f], obj);
+    });
+
+  return obj;
+};
+
